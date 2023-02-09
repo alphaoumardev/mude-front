6e861da6d33930c0219e5d85dbd30aaee3a15d4f
alphaoumardev <alphaoumardev@outlook.com>
2023-02-09 20:49:45 +0800

    Add product

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,56 +1,56 @@
-// import { combineReducers } from 'redux';
-// import authReducer from './authReducer';
-// import {cartReducer} from "./cartReducer";
-// import {wishlistReducer} from './wishlistReducer'
-// import {
-//     create_addressReducer,
-//     getShippingAddressReducer,
-//     orderCreateReducer,
-//     getMyorderReducer,
-//     getReviewReducer
-// } from "./orderReducer";
-// import {
-//     getAllProductsReducer,
-//     getColorsReducer,
-//     getImagesReducer,
-//     getNewProductsReducer,
-//     getOneProductReducer,
-//     getOnsaleProductsReducer,
-//     getProductBySubcategoriesReducer,
-//     getproductByVariantReducer,
-//     getProductsByCatNameReducer,
-//     getProductsByPagegReducer,
-//     getSizesReducer,
-//     getSlidersReducer,
-//     getTagsReducer
-// } from "./productsReducer";
-//
-// export default combineReducers(
-//     {
-//         authReducer,
-//
-//         cartReducer ,
-//         wishlistReducer,
-//
-//         getAllProductsReducer,
-//         getProductsByPagegReducer,
-//         getOneProductReducer,
-//         getProductBySubcategoriesReducer,
-//         getproductByVariantReducer,
-//         getImagesReducer,
-//         getProductsByCatNameReducer,
-//         getColorsReducer,
-//         getSizesReducer,
-//         getTagsReducer,
-//         getNewProductsReducer,
-//         getOnsaleProductsReducer,
-//         getSlidersReducer,
-//
-//         create_addressReducer,
-//         getShippingAddressReducer,
-//
-//         orderCreateReducer,
-//         getMyorderReducer,
-//         getReviewReducer
-//
-//     });
+import { combineReducers } from 'redux';
+import authReducer from './authReducer';
+import {cartReducer} from "./cartReducer";
+import {wishlistReducer} from './wishlistReducer'
+import {
+    create_addressReducer,
+    getShippingAddressReducer,
+    orderCreateReducer,
+    getMyorderReducer,
+    getReviewReducer
+} from "./orderReducer";
+import {
+    getAllProductsReducer,
+    getColorsReducer,
+    getImagesReducer,
+    getNewProductsReducer,
+    getOneProductReducer,
+    getOnsaleProductsReducer,
+    getProductBySubcategoriesReducer,
+    getproductByVariantReducer,
+    getProductsByCatNameReducer,
+    getProductsByPagegReducer,
+    getSizesReducer,
+    getSlidersReducer,
+    getTagsReducer
+} from "./productsReducer";
+
+export default combineReducers(
+    {
+        authReducer,
+
+        cartReducer ,
+        wishlistReducer,
+
+        getAllProductsReducer,
+        getProductsByPagegReducer,
+        getOneProductReducer,
+        getProductBySubcategoriesReducer,
+        getproductByVariantReducer,
+        getImagesReducer,
+        getProductsByCatNameReducer,
+        getColorsReducer,
+        getSizesReducer,
+        getTagsReducer,
+        getNewProductsReducer,
+        getOnsaleProductsReducer,
+        getSlidersReducer,
+
+        create_addressReducer,
+        getShippingAddressReducer,
+
+        orderCreateReducer,
+        getMyorderReducer,
+        getReviewReducer
+
+    });
