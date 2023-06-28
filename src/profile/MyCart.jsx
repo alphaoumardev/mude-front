3a788d81633445b8f0067dd779c0305f66d99e86
alphaoumardiallo <alphaoumardev@outlook.com>
2023-06-28 13:47:16 +0800

    the point

--- src/profile/MyCart.jsx
+++ src/profile/MyCart.jsx
@@ -6,12 +6,12 @@ const MyCart = () =>
 {
     return (
         <div className=" flex justify-center">
-            {/*<div className="hidden sm:flex sm:justify-center mt-20 sm:sticky sm:top-0">*/}
-            {/*<MySideBar/>*/}
-            {/*</div>*/}
-            {/*<div className="sm:w-8/12  lg:grid lg:grid-cols-1">*/}
-            {/*    <MudeCart/>*/}
-            {/*</div>*/}
+            <div className="hidden sm:flex sm:justify-center mt-20 sm:sticky sm:top-0">
+            <MySideBar/>
+            </div>
+            <div className="sm:w-8/12  lg:grid lg:grid-cols-1">
+                <MudeCart/>
+            </div>
         </div>
     )
 }
